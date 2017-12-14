8a844f4500f116d550a286c9fa4b0a2451e129b6
bluelovers <codelovers@users.sourceforge.net>
2017-12-14 15:40:51 +0800

    little change for support plugin

--- index.ts
+++ index.ts
@@ -18,31 +18,35 @@ cssWhat.stringify = require('./css-what/stringify');
  * @type {Boolean} browserEnv
  */
 let browserEnv = false;
-try {
-    document;
-    browserEnv = true;
-} catch (e) {}
+try
+{
+	document;
+	browserEnv = true;
+}
+catch (e)
+{}
 
 /**
  * Save Loaders of different Language
  * @type {Object} store
  */
 let store = {
-    style : {
-        defaults : '',
-        langs : {},
-        exports ( style ) {
-            document.head.appendChild(style);
-        },
-    },
-    script : {
-        defaults : '',
-        langs : {},
-    },
-    template : {
-        defaults : '',
-        langs : {},
-    },
+	style: {
+		defaults: '',
+		langs: {},
+		exports(style)
+		{
+			document.head.appendChild(style);
+		},
+	},
+	script: {
+		defaults: '',
+		langs: {},
+	},
+	template: {
+		defaults: '',
+		langs: {},
+	},
 };
 
 /**
@@ -50,8 +54,9 @@ let store = {
  * @param {String} type | style | script | template |
  * @param {String} lang scss, ts, jade etc.
  */
-function set ( type, lang ) {
-    store[type].defaults = lang;
+function set(type, lang)
+{
+	store[type].defaults = lang;
 }
 
 /**
@@ -60,12 +65,14 @@ function set ( type, lang ) {
  * @param {String} lang scss, ts, jade etc.
  * @param {Function} handler
  */
-function register ( type, lang, handler ) {
-    if (Array.isArray(lang)) {
-        lang.forEach((v) => store[type].langs[v] = handler);
-        return;
-    }
-    store[type].langs[lang] = handler;
+function register(type, lang, handler)
+{
+	if (Array.isArray(lang))
+	{
+		lang.forEach((v) => store[type].langs[v] = handler);
+		return;
+	}
+	store[type].langs[lang] = handler;
 }
 
 /**
@@ -74,126 +81,146 @@ function register ( type, lang, handler ) {
  * @param {String} filePath file path
  * @export {Vue} Vue Component after compile
  */
-function loader ( module, filePath ) {
+function loader(module, filePath)
+{
 
-    let content = fs.readFileSync(filePath, 'utf8');
-    let moduleId = `data-v-${ hash(filePath) }`;
+	let content = fs.readFileSync(filePath, 'utf8');
+	let moduleId = `data-v-${ hash(filePath) }`;
 
-    let vueTemplate = '';
-    let vueComponent = compiler.parseComponent(stripBom(content));
+	let vueTemplate = '';
+	let vueComponent = compiler.parseComponent(stripBom(content));
 
-    let script = vueComponent.script;
-    let styles = vueComponent.styles;
-    let template = vueComponent.template;
+	let script = vueComponent.script;
+	let styles = vueComponent.styles;
+	let template = vueComponent.template;
 
-    let scoped = styles.some(({ attrs }) => attrs.scoped);
+	let scoped = styles.some(({ attrs }) => attrs.scoped);
 
-    [].concat(script, template, styles).forEach(( tag, index ) => {
-        if (tag) {
-            let type = tag.type;
-            let content = tag.content;
-            let lang = tag.attrs.lang || store[type].defaults;
-            let handler = store[type].langs[lang];
-            if (handler) {
-                content = handler(content, filePath, index, module);
-            }
-            switch (type) {
-                case 'style':
-                    if (browserEnv) {
-                        /**
-                         * Only in Browser Environment, append style to head
-                         */
-                        if (tag.attrs.scoped) {
-                            let ast = css.parse(content);
-                            ast.stylesheet.rules.forEach(( rule ) => {
-                                rule.selectors = rule.selectors.map(( selector ) => {
-                                    let [ patterns ] = cssWhat(selector);
-                                    let index = patterns.length - 1;
-                                    for (; index >= 0; index--) {
-                                        let { type } = patterns[index];
-                                        if (type !== 'pseudo' && type !== 'pseudo-element') {
-                                            break;
-                                        }
-                                    }
-                                    patterns.splice(index + 1, 0, {
-                                        value : '',
-                                        name : moduleId,
-                                        action : 'exists',
-                                        type : 'attribute',
-                                        ignoreCase : false,
-                                    });
-                                    return cssWhat.stringify([patterns]);
-                                });
-                            });
-                            content = css.stringify(ast);
-                        }
-                        let style = document.createElement('style');
-                        style.innerHTML = content;
-                        store.style.exports.call(module.exports, style, {
-                            index,
-                            styles,
-                            filePath,
-                        });
-                    }
-                    break;
-                case 'script':
-                    module._compile(content, filePath);
-                    break;
-                case 'template':
-                    if (browserEnv) {
-                        /**
-                         * Only in Browser Environment, set Attribute for each element
-                         */
-                        if (scoped) {
-                            let div = document.createElement('div');
-                            div.innerHTML = content;
-                            let root = div.firstElementChild;
-                            let walk = function walk ( element, handler ) {
-                                handler(element);
-                                let children = element.children || [];
-                                [].forEach.call(children, ( child ) => {
-                                    walk(child, handler);
-                                });
-                            };
-                            walk(root, ( element ) => {
-                                element.setAttribute(moduleId, '');
-                            });
-                            content = div.innerHTML;
-                        }
-                    }
-                    vueTemplate = content;
-                    break;
-            }
-        }
-    });
+	[].concat(script, template, styles).forEach((tag, index) =>
+	{
+		if (tag)
+		{
+			let type = tag.type;
+			let content = tag.content;
+			let lang = tag.attrs.lang || store[type].defaults;
+			let handler = store[type].langs[lang];
+			if (handler)
+			{
+				content = handler(content, filePath, index, module);
+			}
+			switch (type)
+			{
+				case 'style':
+					if (browserEnv)
+					{
+						/**
+						 * Only in Browser Environment, append style to head
+						 */
+						if (tag.attrs.scoped)
+						{
+							let ast = css.parse(content);
+							ast.stylesheet.rules.forEach((rule) =>
+							{
+								rule.selectors = rule.selectors.map((selector) =>
+								{
+									let [patterns] = cssWhat(selector);
+									let index = patterns.length - 1;
+									for (; index >= 0; index--)
+									{
+										let { type } = patterns[index];
+										if (type !== 'pseudo' && type !== 'pseudo-element')
+										{
+											break;
+										}
+									}
+									patterns.splice(index + 1, 0, {
+										value: '',
+										name: moduleId,
+										action: 'exists',
+										type: 'attribute',
+										ignoreCase: false,
+									});
+									return cssWhat.stringify([patterns]);
+								});
+							});
+							content = css.stringify(ast);
+						}
+						let style = document.createElement('style');
+						style.innerHTML = content;
+						store.style.exports.call(module.exports, style, {
+							index,
+							styles,
+							filePath,
+						});
+					}
+					break;
+				case 'script':
+					module._compile(content, filePath);
+					break;
+				case 'template':
+					if (browserEnv)
+					{
+						/**
+						 * Only in Browser Environment, set Attribute for each element
+						 */
+						if (scoped)
+						{
+							let div = document.createElement('div');
+							div.innerHTML = content;
+							let root = div.firstElementChild;
+							let walk = function walk(element, handler)
+							{
+								handler(element);
+								let children = element.children || [];
+								[].forEach.call(children, (child) =>
+								{
+									walk(child, handler);
+								});
+							};
+							walk(root, (element) =>
+							{
+								element.setAttribute(moduleId, '');
+							});
+							content = div.innerHTML;
+						}
+					}
+					vueTemplate = content;
+					break;
+			}
+		}
+	});
 
-    module.exports.vueComponent = vueComponent;
-    module.exports.template = vueTemplate;
+	module.exports.vueComponent = vueComponent;
+	module.exports.template = vueTemplate;
 }
 
 /**
  * Exports API to expand
  */
-['style', 'script', 'template'].forEach(( type ) => {
-    loader[type] = {
-        set ( lang ) {
-            set(type, lang);
-            return this;
-        },
-        register ( lang, handler ) {
-            register(type, lang, handler);
-            return this;
-        },
-    };
+['style', 'script', 'template'].forEach((type) =>
+{
+	loader[type] = {
+		set(lang)
+		{
+			set(type, lang);
+			return this;
+		},
+		register(lang, handler)
+		{
+			register(type, lang, handler);
+			return this;
+		},
+	};
 });
 
 /**
  * Handler of creating styles
  * @param {Function} handler
  */
-loader.style.exports = ( handler ) => {
-    store.style.exports = handler;
-    return this;
+loader.style.exports = (handler) =>
+{
+	store.style.exports = handler;
+	return this;
 };
 
 /**
